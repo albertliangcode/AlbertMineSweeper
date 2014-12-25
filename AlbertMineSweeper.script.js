78c077683b97788f53607926bd953f6e4bf586a4
albertliangcode <albertliangcode@gmail.com>
2014-12-25 13:38:58 -0800

    AMS Code Review Update Added Check Win function Added User Click Response Linted Code

--- AlbertMineSweeper.script.js
+++ AlbertMineSweeper.script.js
@@ -1,5 +1,5 @@
 /* AlbertMineSweeper.script.js
- * Date: 2014-12-23
+ * Date: 2014-12-25
  * Author: Albert Liang
  * Mentor: Adam Yee
  * 
@@ -25,28 +25,36 @@ function Cell() {
 	this.mine = false;
 	this.visited = false;
 	this.risk = 0;
-};
+}
 
 
 /***Initialize Grid***/
 var initialize = function() {
+  var numRow;
+  var numCol;
+  var numMines;
+  var isInsideGrid;
+  var grid;
+  var a;
+  var b;
+  
 	// Prompt user until valid input is recieved
 	while(true) {
-		var numRow = prompt("How many rows? (Minimum 5)");
-		var numCol = prompt("How many columns? (Minimum 5)");
-		var numMines = prompt("How many mines? (Minimum 1)");
-		if (numRow==NaN || numRow<5 || numCol==NaN || numCol<5 || numMines==NaN || numMines<1) {
+		numRow = prompt("How many rows? (Minimum 5)");
+		numCol = prompt("How many columns? (Minimum 5)");
+		numMines = prompt("How many mines? (Minimum 1)");
+		if (isNaN(numRow) || numRow < 5 || isNaN(numCol) || numCol < 5 || isNaN(numMines) || numMines < 1) {
 			alert("Please enter valid numbers for rows, columns, and mines.");
-		} else if (numMines>=numRow*numCol-1) {
+		} else if (numMines >= numRow * numCol - 1) {
 			alert("You are in MINESWEEPER'S ROULETTE mode.");
-			numMines=numRow*numCol-1;	//Just for humor.
+			numMines = numRow * numCol - 1;	//Just for humor.
 		} else {
 			break;
 		}
 	}
 
 	// Generate grid of empty Cell Objects
-	var grid = [];					// !!! Is this correct? !!!
+	grid = [];	 		//!!!Should I just create a Grid class to handle isInsideGrid, numRow, numCol, and unvisited Cells? Would need to find some way of overloading '[]'.!!!
 	for(var i = 0; i < numRow; i++) {
 		grid[i] = [];
 		for(var j = 0; j < numCol; j++) {
@@ -54,23 +62,62 @@ var initialize = function() {
 		}
 	} 
 
-	// Seed mines randomly in grid and calculate each cell's risk
+	// Seed mines randomly in grid, adds them to minedCells[] and calculate each cell's risk
 	for(var c = 0; c < numMines; c++) {
-		var a = Math.floor(numRow*Math.random());
-		var b = Math.floor(numCol*Math.random());
-		if (grid[a][b].mine == true) {
+		a = Math.floor(numRow * Math.random());
+		b = Math.floor(numCol * Math.random());
+		if (grid[a][b].mine === true) {
 			c--;
 			continue;
 		}
 		grid[a][b].mine = true;
 		for(var i = -1; i <= 1; i++) {
-			for(var j = -1; j <=1; j++) {
-				if((a-1>=0 && b-1>=0 && a+1<=numRow && b+1<=numCol) && !(i==0&&j==0)) {
-					grid[a+i][b+j].risk+=1;
+			for(var j = -1; j <= 1; j++) {
+				isInsideGrid = (a + i >= 0 && b + j >= 0 && a + i < numRow && b + j < numCol);
+				if(isInsideGrid && !(i === 0 && j === 0)) {
+					grid[a + i][b + j].risk += 1;
 				}
+			}
+		}
+	}
+	return grid;
+};
+
+
+/***Game Response Each Time User Clicks a Cell***/
+var stomp = function(grid, row, col, numRow, numCol) {
+	var isInsideGrid;
+	grid[row][col].visited = true;
+	//!----->DISPLAY SQUARE<-----!
+	if (grid[row][col].mine === true) {
+		//!----->GAME OVER<-----!
+	} else if (grid[row][col].risk === 0) {
+		for(var i = -1; i <= 1; i++) {
+			for(var j = -1; j <= 1; j++) {
+				isInsideGrid = (a + i >= 0 && b + j >= 0 && a + i < numRow && b + j < numCol);
+				if(isInsideGrid && !(i === 0 && j === 0)) {
+					stomp(grid, row + i, col + j, numRow, numCol);
+				} 
 			}
 		}
 	}
 };
 
 
+/***Check for User Win***/
+var winCheck = function(grid) {
+	for(var i = 0; i < grid.length; i++) {
+		for(var j = 0; j < grid[i].length; j++) {
+			if(!grid[i][j].visited && !grid[i][j].mine) {
+				return false;
+			}
+		}
+	}
+	return true;
+};
+
+
+/***Print Grid***/
+
+
+var grid = initialize();
